79371b25efa17791f6063efb287d8f1d69674bc5
Stas Averin <averin.developer@gmail.com>
2024-03-07 12:50:08 +0400

    Show the list oh coins

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,6 +1,11 @@
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import dev.icerock.moko.mvvm.compose.getViewModel
@@ -28,11 +33,40 @@ fun App() {
         val coinListViewModel = getViewModel(Unit, viewModelFactory { CoinListViewModel() })
         val uiState by coinListViewModel.uiState.collectAsState()
         LaunchedEffect(coinListViewModel) { coinListViewModel.updateCoins() }
-        CoinListScreen(uiState, onCoinSelected = {  })
+        CoinListScreen(uiState, onCoinSelected = { coinId -> println("coinId=$coinId") })
     }
 }
 
 @Composable
 fun CoinListScreen(uiState: CoinListUiState, onCoinSelected: (id: String) -> Unit) {
     println("${uiState.coins.toString()}")
+
+    Column(
+        Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+    ) {
+
+        Text(text = "CoinTracker")
+
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(5.dp),
+            modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp)
+        ) {
+            items(
+                count = uiState.coins.size,
+                itemContent = { CoinItem(uiState.coins[it]) }
+            )
+        }
+    }
+}
+
+@Composable
+fun CoinItem(coin: Coin) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(5.dp),
+        modifier = Modifier.fillMaxWidth().padding(vertical = 16.dp),
+        ) {
+        Text(text = "${coin.name}: $${coin.priceUsd}")
+    }
 }
